5e0761395573af1c3b3a1906e43bebc92053b6d4
Tiffany Bryan <tiffanym.bryan@gmail.com>
2022-05-23 01:02:07 -0400

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,23 +7,23 @@ const Navbar = ({currentPage, handlePageChange}) => {
         <div>
         <nav className="navbar navbar-expand-md navbar-light">
             <div className='container'>
-                <a className="navbar-brand ml-auto" href="#">Tiffany Bryan</a>
+                <a className="navbar-brand ml-auto" href="#"><span>Tiffany Bryan</span></a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <FontAwesomeIcon icon={faBars} style={{color:"white"}} />
                 </button>
                 <div className="collapse navbar-collapse ml-auto" id="navbarNav">
                     <ul className="navbar-nav ml-auto">
                         <li className="nav-item active p-4">
-                            <a className="nav-link " href="#about" onClick ={() => handlePageChange("About") }>About Me</a>
+                            <a className="nav-link " href="#about" onClick ={() => handlePageChange("About") }><span>About Me</span></a>
                         </li>
                         <li className="nav-item p-4">
-                            <a className="nav-link" href="#portfolio" onClick={() => handlePageChange('Portfolio')}>Protfolio</a>
+                            <a className="nav-link" href="#portfolio" onClick={() => handlePageChange('Portfolio')}><span>Protfolio</span></a>
                         </li>
                         <li className="nav-item p-4">
-                            <a className="nav-link" href="#resume" onClick={()=> handlePageChange('Resume')}>Resume</a>
+                            <a className="nav-link" href="#resume" onClick={()=> handlePageChange('Resume')}><span>Resume</span></a>
                         </li>
                         <li className="nav-item p-4">
-                            <a className="nav-link" href="#contact" onClick={()=> handlePageChange('Contact')}>Contact</a>
+                            <a className="nav-link" href="#contact" onClick={()=> handlePageChange('Contact')}><span>Contact</span></a>
                         </li>
                     </ul>
                 </div>
